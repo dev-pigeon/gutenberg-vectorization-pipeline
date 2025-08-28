4821a66b716d6921c1f79fac9650686cb3ca0f4b
Jack Young <jdydev2003@gmail.com>
2025-08-28 14:51:07 -0700

    task class

---
+++ src/task.py
@@ -0,0 +1,18 @@
+class ParseTask:
+    def __init__(self, input: str, output: str):
+        self.input_path = input
+        self.output_path = output
+
+    def process(self):
+        """Processes the file at input_path by cleaning it, lementing it, chunking it, and storing it in output_path"""
+        pass
+
+
+class VectorizeTask:
+    def __init__(self, input: str, output: str):
+        self.input_path = input
+        self.chroma_path = output
+
+    def process(self):
+        """Vectorizes the chunk at input path and stores in chroma_path"""
+        pass
