86453b04c882203d8f4e0b9671cebe94e087c259
Jack Young <jdydev2003@gmail.com>
2025-08-30 14:22:21 -0700

    accounts for no line end

--- src/parser.py
+++ src/parser.py
@@ -3,8 +3,14 @@ import regex
 
 def extractAuthor(header: str):
     match = regex.search(r"Author:", header)
+
     if match:
+        author = ""
         line_end = header.find("\n", match.end())
-        author = header[match.end():line_end].strip()
+        if line_end == -1:
+            author = header[match.end():].strip()
+        else:
+            author = header[match.end():line_end].strip()
         return author
+
     return "Unknown"
