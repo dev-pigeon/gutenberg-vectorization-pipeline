21da10636bac0a71c2d54ab3d53e1885e8f707ee
Jack Young <jdydev2003@gmail.com>
2025-09-02 16:45:14 -0700

    metadate is set properly

---
+++ src/test/test_chunk_file.py
@@ -0,0 +1,19 @@
+import unittest
+from task import ParseTask
+from parser import Parser
+
+
+class TestChunkFile(unittest.TestCase):
+
+    parser = Parser()
+
+    def test_sets_properties(self):
+        task = ParseTask(
+            "/Users/jackyoungadmin/Documents/Projects/vectorization/text-vectorization-pipeline/data/books/oscar_wilde.txt")
+        expected_author = "Oscar Wilde"
+        expected_title = "The Picture of Dorian Gray"
+        expected_rd = "October 1, 1994"
+        self.parser.chunk_file(task)
+        self.assertEqual(expected_author, self.parser.author)
+        self.assertEqual(expected_title, self.parser.title)
+        self.assertEqual(expected_rd, self.parser.release_date)
