01e21b800a325b425626525f831b8d4af923dddf
Jack Young <jdydev2003@gmail.com>
2025-09-02 16:22:20 -0700

    removed extra text

--- src/test/test_extract_header.py
+++ src/test/test_extract_header.py
@@ -54,39 +54,7 @@ Credits: Produced by Greg Weeks, Mary Meehan and the Online
         _PRINTED FOR_
         LACKINGTON, HUGHES, HARDING, MAVOR, & JONES,
         FINSBURY SQUARE.
-
-
-        This ebook is for the use of anyone anywhere in the United States and
-most other parts of the world at no cost and with almost no restrictions
-whatsoever. You may copy it, give it away or re-use it under the terms
-of the Project Gutenberg License included with this ebook or online
-at www.gutenberg.org. If you are not located in the United States,
-you will have to check the laws of the country where you are located
-before using this eBook.
-
- This ebook is for the use of anyone anywhere in the United States and
-most other parts of the world at no cost and with almost no restrictions
-whatsoever. You may copy it, give it away or re-use it under the terms
-of the Project Gutenberg License included with this ebook or online
-at www.gutenberg.org. If you are not located in the United States,
-you will have to check the laws of the country where you are located
-before using this eBook.
-
- This ebook is for the use of anyone anywhere in the United States and
-most other parts of the world at no cost and with almost no restrictions
-whatsoever. You may copy it, give it away or re-use it under the terms
-of the Project Gutenberg License included with this ebook or online
-at www.gutenberg.org. If you are not located in the United States,
-you will have to check the laws of the country where you are located
-before using this eBook.
-
- This ebook is for the use of anyone anywhere in the United States and
-most other parts of the world at no cost and with almost no restrictions
-whatsoever. You may copy it, give it away or re-use it under the terms
-of the Project Gutenberg License included with this ebook or online
-at www.gutenberg.org. If you are not located in the United States,
-you will have to check the laws of the country where you are located
-before using this eBook.
+        
 """
 
     valid_expected = """The Project Gutenberg eBook of Frankenstein; Or, The Modern Prometheus
