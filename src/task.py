c658f64b1e4c04fc2615b66cea1e10c8be1e7317
Jack Young <jdydev2003@gmail.com>
2025-09-09 10:51:49 -0700

    removed vectorize task

--- src/task.py
+++ src/task.py
@@ -5,13 +5,3 @@ class ParseTask:
     def process(self):
         """Processes the file at input_path by cleaning it, lementing it, chunking it, and storing it in output_path"""
         pass
-
-
-class VectorizeTask:
-    def __init__(self, input: str, output: str):
-        self.input_path = input
-        self.chroma_path = output
-
-    def process(self):
-        """Vectorizes the chunk at input path and stores in chroma_path"""
-        pass
